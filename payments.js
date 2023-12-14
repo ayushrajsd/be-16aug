3a8f8cfdd7d177413c8b478285ec37e63febefd8
Scaler <scaler@Scalers-MacBook-Pro.local>
2023-12-14 07:46:50 +0530

    lecture 9 payments

---
+++ payments.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const shortid = require("shortid");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+var instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_SECRET_KEY,
+  });
+
+  app.post('/checkout', (req, res) => {
+    // the details below in a real world app will be fetched via an internal order id
+    // when the user finalized their cart, an order id will be generated and passed to this route
+      var options = {
+        amount: 11000,  // amount in the smallest currency unit
+        currency: "INR",
+        receipt: shortid.generate(),
+      };
+      instance.orders.create(options, function(err, order) {
+        console.log(order);
+        res.status(200).json({
+            message:"Order created",
+            data:order
+        })
+      });
+  })
+
+  app.post('/verification', (req, res) => {
+        try{
+          console.log("webhook called",req.body);
+          res.json({status:"ok"})
+
+        }catch(err){
+            console.log(err);
+        }
+  })
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
